2ef1e4aa1759a761e1ad132a580767134bf65f7e
Bryan Tremblay <bryanandhallie@gmail.com>
2017-05-02 14:32:08 -0400

    Made some changes to GOGOGO.bat that allow it to properly close instead of mysteriously staying open for some reason

--- GOGOGO.bat
+++ GOGOGO.bat
@@ -1,1 +1,4 @@
-start code Project/
+:: I should have been able to run 'start code Project/'
+:: However something odd is happening that keeps CMD open
+:: So I've resorted to manually calling VSCode like this
+start "" "C:\Program Files (x86)\Microsoft VS Code\Code.exe" Project\
