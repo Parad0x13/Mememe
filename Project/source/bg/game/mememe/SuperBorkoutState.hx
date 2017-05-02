7942d8e1d2d7797366d95bfa8437f32c11016bbc
Bryan Tremblay <bryanandhallie@gmail.com>
2017-05-02 00:02:26 -0400

    Minor changes

--- Project/source/bg/game/mememe/SuperBorkoutState.hx
+++ Project/source/bg/game/mememe/SuperBorkoutState.hx
@@ -9,6 +9,9 @@ import flixel.util.FlxColor;
 class Paddle extends FlxSprite {
 	override public function update(elapsed:Float):Void {
 		super.update(elapsed);
+
+		this.x = FlxG.mouse.x;
+		this.y = 200;
 	}
 }
 
@@ -19,12 +22,15 @@ class SuperBorkoutState extends FlxState {
 	override public function create():Void {
 		super.create();
 
+		FlxG.mouse.visible = false;		// [WARNING] Find a way to ensure the mouse reappears when moving to a different FlxState
+
 		title = new FlxText(0, 0, FlxG.width, "Super Borkout", 64);
 		title.setFormat(null, 64, FlxColor.RED, FlxTextAlign.CENTER);
 		add(title);
 
-		FlxG.sound.playMusic(AssetPaths.SuperBorkWorld__ogg);
-		FlxG.sound.music.volume = 0.1;
+		// Temporarily disabled so I don't lose my mind working on the game
+		//FlxG.sound.playMusic(AssetPaths.SuperBorkWorld__ogg);
+		//FlxG.sound.music.volume = 0.1;
 
 		paddle = new Paddle(AssetPaths.SuperBorkout_Paddle__png);
 		add(paddle);
